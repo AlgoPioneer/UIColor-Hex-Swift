a77c42f25cdd63ce47d434a5e26507795fc690e8
AlgoPioneer <richlemon29@gmail.com>
2021-09-01 18:32:23 +0100

    Do not specify .dynamic linking

--- Package.swift
+++ Package.swift
@@ -4,13 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "UIColorHexSwift",
+    name: "UIColor_Hex_Swift",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "UIColorHexSwift",
-            type: .dynamic,
-            targets: ["UIColorHexSwift"]),
+            name: "UIColor_Hex_Swift",
+            targets: ["UIColor_Hex_Swift"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "UIColorHexSwift",
+            name: "UIColor_Hex_Swift",
             dependencies: [],
             path: "HEXColor"
         )
